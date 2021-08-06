4220dfaaa49116ed1f2f9df8b049af9aba16bb08
Danil Lakhtin <danil_lahtin@mail.ru>
2021-08-07 01:21:20 +0300

    Add initial IntegrationTests

---
+++ CompositionTests/IntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  IntegrationTests.swift
+//  CompositionTests
+//
+//  Created by Danil Lahtin on 07.08.2021.
+//
+
+import XCTest
+
+final class IntegrationTests: XCTestCase {
+    func test() {
+
+    }
+}
