b60f227d7de4e871021d06d29764b0641215356f
Danil Lakhtin <danil_lahtin@mail.ru>
2021-08-07 00:02:08 +0300

    Add Multiplatform project

---
+++ Multiplatform/Multiplatform.h
@@ -0,0 +1,18 @@
+//
+//  Multiplatform.h
+//  Multiplatform
+//
+//  Created by Danil Lahtin on 06.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Multiplatform.
+FOUNDATION_EXPORT double MultiplatformVersionNumber;
+
+//! Project version string for Multiplatform.
+FOUNDATION_EXPORT const unsigned char MultiplatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Multiplatform/PublicHeader.h>
+
+
