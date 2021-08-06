d075a25c6ca7216d79e3e8587402f5e46d2641e4
Danil Lakhtin <danil_lahtin@mail.ru>
2021-08-07 01:06:37 +0300

    Add Presentation target

---
+++ PresentationTests/PresentationTests.swift
@@ -0,0 +1,15 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by Danil Lahtin on 07.08.2021.
+//
+
+import XCTest
+@testable import Presentation
+
+class PresentationTests: XCTestCase {
+    func test() {
+
+    }
+}
