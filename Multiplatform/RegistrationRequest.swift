d62556203aed55d8f4dda0aceaf696a26b1c1c2f
Danil Lakhtin <danil_lahtin@mail.ru>
2021-08-07 00:04:11 +0300

    Add RegistrationRequest

---
+++ Multiplatform/RegistrationRequest.swift
@@ -0,0 +1,11 @@
+//
+//  RegistrationRequest.swift
+//  Multiplatform
+//
+//  Created by Danil Lahtin on 07.08.2021.
+//
+
+public struct RegistrationRequest {
+    let username: String?
+    let password: String?
+}
