a4b622abd023609001df9acd941dfcbfe96a6ae7
Danil Lakhtin <danil_lahtin@mail.ru>
2021-08-07 01:10:19 +0300

    Add initial RegistrationPresenterTests

---
+++ PresentationTests/RegistrationPresenterTests.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationPresenterTests.swift
+//  PresentationTests
+//
+//  Created by Danil Lahtin on 07.08.2021.
+//
+
+import XCTest
+
+final class RegistrationPresenter {
+    
+}
+
+final class RegistrationPresenterTests: XCTestCase {
+    func test_init() {
+        _ = RegistrationPresenter()
+    }
+}
