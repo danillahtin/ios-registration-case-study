5ba2723d2b118c7df6b42629cf6610b8b9e2cffd
Danil Lakhtin <danil_lahtin@mail.ru>
2021-08-07 00:14:40 +0300

    Add initial RequestValidatingDecoratorTests

---
+++ CoreTests/RequestValidatingDecoratorTests.swift
@@ -0,0 +1,18 @@
+//
+//  RequestValidatingDecoratorTests.swift
+//  CoreTests
+//
+//  Created by Danil Lahtin on 07.08.2021.
+//
+
+import XCTest
+
+final class RequestValidatingDecorator {
+    
+}
+
+final class RequestValidatingDecoratorTests: XCTestCase {
+    func test_init() {
+        let sut = RequestValidatingDecorator()
+    }
+}
