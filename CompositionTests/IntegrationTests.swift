d56b852655a2689d3090f431f56e6dee2032b606
Danil Lakhtin <danil_lahtin@mail.ru>
2021-08-07 20:52:50 +0300

    Load view displays empty username

--- CompositionTests/IntegrationTests.swift
+++ CompositionTests/IntegrationTests.swift
@@ -6,9 +6,42 @@
 //
 
 import XCTest
+import UIKit
+
+final class RegistrationViewController: UIViewController {
+    weak var usernameTextField: UITextField!
+
+    override func loadView() {
+        let view = UIView()
+
+        let usernameTextField = UITextField()
+
+        view.addSubview(usernameTextField)
+
+        self.usernameTextField = usernameTextField
+        self.view = view
+    }
+}
 
 final class IntegrationTests: XCTestCase {
-    func test() {
+    func test_loadView_displaysEmptyUsername() {
+        let sut = makeSut()
 
+        XCTAssertEqual(sut.username, "")
+    }
+
+    // MARK: - Helpers
+    private func makeSut() -> RegistrationViewController {
+        let sut = RegistrationViewController()
+
+        sut.loadViewIfNeeded()
+        
+        return sut
+    }
+}
+
+private extension RegistrationViewController {
+    var username: String? {
+        usernameTextField.text
     }
 }
