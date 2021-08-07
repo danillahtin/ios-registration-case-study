e1d86dd055838f5830696f0502fe03b0cac3352c
Danil Lakhtin <danil_lahtin@mail.ru>
2021-08-07 20:55:25 +0300

    Load view displays empty password

--- CompositionTests/IntegrationTests.swift
+++ CompositionTests/IntegrationTests.swift
@@ -10,15 +10,19 @@ import UIKit
 
 final class RegistrationViewController: UIViewController {
     weak var usernameTextField: UITextField!
+    weak var passwordTextField: UITextField!
 
     override func loadView() {
         let view = UIView()
 
         let usernameTextField = UITextField()
+        let passwordTextField = UITextField()
 
         view.addSubview(usernameTextField)
+        view.addSubview(passwordTextField)
 
         self.usernameTextField = usernameTextField
+        self.passwordTextField = passwordTextField
         self.view = view
     }
 }
@@ -30,12 +34,18 @@ final class IntegrationTests: XCTestCase {
         XCTAssertEqual(sut.username, "")
     }
 
+    func test_loadView_displaysEmptyPassword() {
+        let sut = makeSut()
+
+        XCTAssertEqual(sut.password, "")
+    }
+
     // MARK: - Helpers
     private func makeSut() -> RegistrationViewController {
         let sut = RegistrationViewController()
 
         sut.loadViewIfNeeded()
-        
+
         return sut
     }
 }
@@ -43,5 +53,9 @@ final class IntegrationTests: XCTestCase {
 private extension RegistrationViewController {
     var username: String? {
         usernameTextField.text
+    }
+
+    var password: String? {
+        passwordTextField.text
     }
 }
