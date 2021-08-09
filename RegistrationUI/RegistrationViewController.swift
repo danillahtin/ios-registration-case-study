aae07aa27e59cf4ed001b1f908c5e998c79faf53
Danil Lakhtin <danil_lahtin@mail.ru>
2021-08-10 00:03:20 +0300

    Add ErrorViewControllerTests

--- RegistrationUI/RegistrationViewController.swift
+++ RegistrationUI/RegistrationViewController.swift
@@ -14,59 +14,6 @@ public protocol RegistrationViewControllerDelegate {
     func didCancelInput()
 }
 
-public final class ErrorViewController: UIViewController {
-    public weak var errorView: UIButton! { view as? UIButton }
-    private var animator: Animator!
-
-    public static func make(animator: Animator) -> ErrorViewController {
-        let vc = ErrorViewController()
-        vc.animator = animator
-
-        return vc
-    }
-
-    public override func loadView() {
-        let errorView = UIButton()
-        errorView.contentEdgeInsets = .init(top: 24, left: 16, bottom: 24, right: 16)
-        errorView.setTitleColor(.white, for: .normal)
-        errorView.backgroundColor = .systemPink
-        errorView.titleLabel?.numberOfLines = 0
-        errorView.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
-        errorView.titleLabel?.textAlignment = .center
-        errorView.addTarget(self, action: #selector(onErrorViewTapped), for: .touchUpInside)
-
-        view = errorView
-    }
-
-    @objc
-    private func onErrorViewTapped() {
-        hideErrorView()
-    }
-
-    private func hideErrorView() {
-        animator.animate { [weak self] in
-            self?.errorView.alpha = 0
-        }
-    }
-
-    private func showErrorView(message: String) {
-        errorView.setTitle(message, for: .normal)
-        animator.animate { [weak self] in
-            self?.errorView.alpha = 1
-        }
-    }
-}
-
-extension ErrorViewController: ErrorView {
-    public func display(viewModel: ErrorViewModel) {
-        if let message = viewModel.message {
-            showErrorView(message: message)
-        } else {
-            hideErrorView()
-        }
-    }
-}
-
 public final class RegistrationViewController: UIViewController {
     public typealias TapGestureRecognizerFactory = (_ target: Any?, _ action: Selector?) -> UITapGestureRecognizer
 
---
+++ RegistrationUITests/ErrorViewControllerTests.swift
@@ -0,0 +1,82 @@
+//
+//  ErrorViewControllerTests.swift
+//  RegistrationUITests
+//
+//  Created by Danil Lahtin on 09.08.2021.
+//
+
+import XCTest
+import XCTestHelpers
+import RegistrationUI
+
+final class ErrorViewControllerTests: XCTestCase {
+    func test_givenErrorViewIsDisplayed_whenErrorViewButtonTapped_thenErrorViewIsNotDisplayed() {
+        let sut = makeSut()
+
+        XCTAssertEqual(sut.isErrorViewDisplayed, true)
+
+        sut.simulateErrorViewTapped()
+        XCTAssertEqual(sut.isErrorViewDisplayed, false)
+    }
+
+    func test_displayErrorViewModel_updatesErrorMessage() {
+        let sut = makeSut()
+
+        sut.display(viewModel: .init(message: "some message"))
+        XCTAssertEqual(sut.errorMessage, "some message")
+
+        sut.display(viewModel: .init(message: "another message"))
+        XCTAssertEqual(sut.errorMessage, "another message")
+    }
+
+    func test_displayErrorViewModel_updatesErrorViewVisibility() {
+        let sut = makeSut()
+
+        sut.display(viewModel: .init(message: "some message"))
+        XCTAssertEqual(sut.isErrorViewDisplayed, true)
+
+        sut.display(viewModel: .init(message: nil))
+        XCTAssertEqual(sut.isErrorViewDisplayed, false)
+
+        sut.display(viewModel: .init(message: "another message"))
+        XCTAssertEqual(sut.isErrorViewDisplayed, true)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSut(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> ErrorViewController {
+        let animator = ImmediateAnimator()
+        let sut = ErrorViewController.make(animator: animator)
+
+        sut.loadViewIfNeeded()
+
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(animator, file: file, line: line)
+
+        return sut
+    }
+}
+
+private extension ErrorViewController {
+    var isErrorViewDisplayed: Bool {
+        errorView.alpha > 0
+    }
+
+    var errorMessage: String? {
+        errorView.title(for: .normal)
+    }
+
+    func simulateErrorViewTapped() {
+        errorView.simulateTap()
+    }
+}
+
+private final class ImmediateAnimator: Animator {
+    func animate(_ animations: @escaping () -> (), completion: (() -> ())?) {
+        animations()
+        completion?()
+    }
+}
