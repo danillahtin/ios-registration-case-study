7c45d5545afbbafa7140b72c2485b954bffac7b7
Danil Lakhtin <danil_lahtin@mail.ru>
2021-08-10 02:08:26 +0300

    Add FormViewLayoutSnapshotTests

---
+++ RegistrationUITests/FormViewLayoutSnapshotTests.swift
@@ -0,0 +1,41 @@
+//
+//  FormViewLayoutSnapshotTests.swift
+//  RegistrationUITests
+//
+//  Created by Danil Lahtin on 10.08.2021.
+//
+
+import XCTest
+import SnapshotTesting
+import RegistrationUI
+
+final class FormViewLayoutSnapshotTests: XCTestCase {
+    func test() {
+        assertSnapshot(sut: makeSut())
+    }
+
+    // MARK: - Helpers
+
+    private func makeSut() -> UIViewController {
+        let container = makeView(name: "container", color: .white)
+        let form = makeView(name: "form", color: .blue, height: 300)
+
+        FormViewLayout(container: container, form: form).apply()
+
+        let sut = UIViewController()
+        sut.view = container
+
+        return sut
+    }
+
+    private func makeView(name: String, color: UIColor, height: CGFloat? = nil) -> UIView {
+        let label = UILabel()
+        label.backgroundColor = color
+        label.text = name
+        label.textAlignment = .center
+        label.textColor = .black
+        height.map({ label.heightAnchor.constraint(equalToConstant: $0).isActive = true })
+
+        return label
+    }
+}
