23bae36b4a76ff6a49edbf95940f88367a2d0602
Danil Lakhtin <danil_lahtin@mail.ru>
2021-08-15 12:46:10 +0300

    Add SocialsPickerViewController tests

---
+++ RegistrationUITests/SocialsPickerViewControllerTests.swift
@@ -0,0 +1,48 @@
+//
+//  SocialsPickerViewControllerTests.swift
+//  RegistrationUITests
+//
+//  Created by Danil Lahtin on 15.08.2021.
+//
+
+import XCTest
+import XCTestHelpers
+import RegistrationUI
+
+final class SocialsPickerViewControllerTests: XCTestCase {
+    func test_whenAppleButtonTapped_thenCallbackIsCalled() {
+        let sut = makeSut()
+
+        var callCount = 0
+        sut.didTapAppleButton = { callCount += 1 }
+
+        XCTAssertEqual(callCount, 0)
+
+        sut.simulateAppleButtonTapped()
+        XCTAssertEqual(callCount, 1)
+
+        sut.simulateAppleButtonTapped()
+        XCTAssertEqual(callCount, 2)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSut(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> SocialsPickerViewController {
+        let sut = SocialsPickerViewController()
+
+        sut.loadViewIfNeeded()
+
+        trackForMemoryLeaks(sut, file: file, line: line)
+
+        return sut
+    }
+}
+
+private extension SocialsPickerViewController {
+    func simulateAppleButtonTapped() {
+        apple.simulateTap()
+    }
+}
